dec4a4f8ac352cb95b9fa5d102843af638ceed82
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-06-23 17:48:38 -0400

    feat: agregar carpeta de microservicio funciones

---
+++ msvc-funciones/src/main/java/com/cine/msvc_funciones/MsvcFuncionesApplication.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_funciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcFuncionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcFuncionesApplication.class, args);
+	}
+
+}
---
+++ msvc-funciones/src/test/java/com/cine/msvc_funciones/MsvcFuncionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_funciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcFuncionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
