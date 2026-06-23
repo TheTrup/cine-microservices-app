01afc1bff48017b6a2024e3628e2cb9c40dd2b58
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-06-23 17:50:46 -0400

    feat: agregar carpeta de microservicio comentarios

---
+++ msvc-comentarios/src/main/java/com/cine/msvc_comentarios/MsvcComentariosApplication.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_comentarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcComentariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcComentariosApplication.class, args);
+	}
+
+}
---
+++ msvc-comentarios/src/test/java/com/cine/msvc_comentarios/MsvcComentariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_comentarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcComentariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
