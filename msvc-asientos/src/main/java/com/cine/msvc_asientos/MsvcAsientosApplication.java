a4ca61b8540d3a4b8b86189169ef6d4f0d98c5ce
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-06-23 17:49:06 -0400

    feat: agregar carpeta de microservicio asientos

---
+++ msvc-asientos/src/main/java/com/cine/msvc_asientos/MsvcAsientosApplication.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_asientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcAsientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcAsientosApplication.class, args);
+	}
+
+}
---
+++ msvc-asientos/src/test/java/com/cine/msvc_asientos/MsvcAsientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_asientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcAsientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
