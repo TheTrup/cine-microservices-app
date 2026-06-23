17d8e861d4c651ff10ea8da5cbfcd9ead20a0c07
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-06-23 17:51:23 -0400

    feat: agregar carpeta de microservicio promociones

---
+++ msvc-promociones/src/main/java/com/cine/msvc_promociones/MsvcPromocionesApplication.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_promociones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcPromocionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcPromocionesApplication.class, args);
+	}
+
+}
---
+++ msvc-promociones/src/test/java/com/cine/msvc_promociones/MsvcPromocionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_promociones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcPromocionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
