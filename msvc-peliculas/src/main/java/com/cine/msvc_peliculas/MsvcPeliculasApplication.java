fbc5fc7f9ef983e5df7ca73edd8f48539e01d3b6
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-06-23 17:43:41 -0400

    feat: agregar carpeta de microservicio peliculas

---
+++ msvc-peliculas/src/main/java/com/cine/msvc_peliculas/MsvcPeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_peliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcPeliculasApplication.class, args);
+	}
+
+}
---
+++ msvc-peliculas/src/test/java/com/cine/msvc_peliculas/MsvcPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_peliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
