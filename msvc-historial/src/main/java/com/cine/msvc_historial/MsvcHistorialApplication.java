d327e6ea5b59363f182eb714a3bd5aa7a222420f
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-06-23 17:51:47 -0400

    feat: agregar carpeta de microservicio historial

---
+++ msvc-historial/src/main/java/com/cine/msvc_historial/MsvcHistorialApplication.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_historial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcHistorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcHistorialApplication.class, args);
+	}
+
+}
---
+++ msvc-historial/src/test/java/com/cine/msvc_historial/MsvcHistorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_historial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcHistorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
