73b887b8646de7a99c4a2324c51bfbb1efff8c4e
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-06-23 17:49:26 -0400

    feat: agregar carpeta de microservicio usuarios

---
+++ msvc-usuarios/src/test/java/com/cine/msvc_usuarios/MsvcUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
