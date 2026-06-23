c54e33af24ec66df5b14fde5934bed09715c1182
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-06-23 17:49:55 -0400

    feat: agregar carpeta de microservicio sucursales

---
+++ msvc-sucursales/src/main/java/com/cine/msvc_sucursales/MsvcSucursalesApplication.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_sucursales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcSucursalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcSucursalesApplication.class, args);
+	}
+
+}
---
+++ msvc-sucursales/src/test/java/com/cine/msvc_sucursales/MsvcSucursalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_sucursales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcSucursalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
