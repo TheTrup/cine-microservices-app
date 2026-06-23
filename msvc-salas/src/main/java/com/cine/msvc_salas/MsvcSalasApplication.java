154c4ee642326a97f87ffb627a10213cc55af719
FleshTLing <102740174+FleshTLing@users.noreply.github.com>
2026-06-23 17:45:49 -0400

    feat: agregar carpeta de microservicio salas

---
+++ msvc-salas/src/main/java/com/cine/msvc_salas/MsvcSalasApplication.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_salas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcSalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcSalasApplication.class, args);
+	}
+
+}
---
+++ msvc-salas/src/test/java/com/cine/msvc_salas/MsvcSalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.msvc_salas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcSalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
